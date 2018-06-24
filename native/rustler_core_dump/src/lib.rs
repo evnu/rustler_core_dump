057db9ee1f393f84877e3d13a614d54819cd96b5
Magnus Ottenklinger <evnu@posteo.de>
2018-06-24 11:45:25 +0200

    Term::to_binary() now returns an OwnedBinary

--- native/rustler_core_dump/src/lib.rs
+++ native/rustler_core_dump/src/lib.rs
@@ -15,7 +15,7 @@ fn roundtrip<'a>(env: Env<'a>, args: &[Term<'a>]) -> NifResult<Term<'a>> {
     let original: Term = args[0].decode()?;
     eprintln!("{:?}", original);
     let binary = original.to_binary();
-    let roundtripped: Term = binary.to_term(env);
+    let roundtripped: Term = binary.release(env).to_term(env);
     eprintln!("{:?}", roundtripped);
     Ok((1, roundtripped).encode(env))
 }
